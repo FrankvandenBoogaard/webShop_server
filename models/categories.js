0b81a771f4fa06549ba755ca3be796720d835224
FrankvandenBoogaard <frank@exprimo.nl>
2022-05-03 00:08:12 +0200

    corrected relation to Reader: ONE category has MANY products

--- models/categories.js
+++ models/categories.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      categorie.belongsTo(models.product);
+      categorie.hasMany(models.product);
 
     }
   }
--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      product.hasMany(models.categories);
+      product.belongsTo(models.categories);
 
     }
   }
