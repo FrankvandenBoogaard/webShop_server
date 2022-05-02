aaadfcd83b16f0c657623a92845af39a2e28330d
FrankvandenBoogaard <frank@exprimo.nl>
2022-05-03 00:35:28 +0200

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const categoriesRouter = require('./routers/categoriesRouter');
 const productsRouter = require('./routers/productsRouter');
 const usersRouter = require('./routers/usersRouter');
 const app = express();
 const PORT = 4000;
 
+app.use(cors())
 app.use(express.json());
+
 
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
